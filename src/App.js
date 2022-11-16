a9c7e43138a73031021a9b5e063f8e2adddb885e
Saleck-Eljili <salek.eljili@gmail.com>
2022-11-16 15:11:26 +0100

    feat: card for the first country

--- src/App.js
+++ src/App.js
@@ -3,11 +3,30 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
 function App() {
+  const variant = "Info";
   return (
     <div>
       <Card className="text-center">
         <Card.Header>Featured</Card.Header>
         <Card.Body>
+          <div>
+            <Card
+              bg={variant.toLowerCase()}
+              key={variant}
+              text={variant.toLowerCase() === "light" ? "dark" : "white"}
+              style={{ width: "18rem" }}
+              className="mb-2"
+            >
+              <Card.Header>Header</Card.Header>
+              <Card.Body>
+                <Card.Title>{variant} Card Title </Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
         <Card.Footer className="text-muted">2 days ago</Card.Footer>
