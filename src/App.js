3920bd23f5f300a6dbc57d6ee2029aa8e727f0a3
Saleck-Eljili <salek.eljili@gmail.com>
2022-11-16 15:08:22 +0100

    feat: init style cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 function App() {
-  return <div className="App">world cup 2022</div>;
+  return (
+    <div>
+      <Card className="text-center">
+        <Card.Header>Featured</Card.Header>
+        <Card.Body>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+        <Card.Footer className="text-muted">2 days ago</Card.Footer>
+      </Card>
+    </div>
+  );
 }
 
 export default App;
